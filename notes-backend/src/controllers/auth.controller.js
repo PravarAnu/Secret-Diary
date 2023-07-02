b5f82b13296cba476bb4d622ff4a459fb7a9e209
Pravar Anu <pravaranu6@gmail.com>
2023-07-02 15:51:05 +0530

    Some little changes

--- notes-backend/src/controllers/auth.controller.js
+++ notes-backend/src/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import customError from "../utils/customError.util.js";
 
 
 const cookieOptions = {
-    expires: new Date(Date.now()+3*24*60*60*1000);
+    expires: new Date(Date.now()+3*24*60*60*1000),
     httpOnly: true
 }
 
