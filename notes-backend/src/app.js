281c393d2563d668ced4aa559075f897581428f5
Pravar Anu <pravaranu6@gmail.com>
2023-07-04 00:54:59 +0530

    Route setup is completed

--- notes-backend/src/app.js
+++ notes-backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import routes from "./routes/index.route.js";
+
 const app = express();
 
 app.use(express.json());
@@ -9,5 +11,18 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 app.use(cookieParser());
 
+
+app.use("/diary/v1", routes);
+
+app.get("/", (req,res)=>{
+    res.send("You are at long route please visit /diary/v1/...");
+})
+
+app.all("*", (req,res)=>{
+    return res.status(404).json({
+        success: false,
+        message: "You are at wrong route"
+    })
+})
 
 export default app;
